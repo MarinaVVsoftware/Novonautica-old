6920a13617adea0d13d0c013559ecabaf690138d
Luiz <lkiora.a@hotmail.es>
2017-09-21 18:15:25 -0500

    agregado entidades del cliente, el barco, y los motores, relación entre barco y motor

---
+++ src/AppBundle/Entity/Barco.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Barco
+ *
+ * @ORM\Table(name="barco")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BarcoRepository")
+ */
+class Barco
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="marca", type="string", length=100, nullable=true)
+     */
+    private $marca;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="modelo", type="string", length=100, nullable=true)
+     */
+    private $modelo;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="anio", type="datetime", nullable=true)
+     */
+    private $anio;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="estatus", type="boolean")
+     */
+    private $estatus;
+
+    /**
+     * One Product has Many Features.
+     * @ORM\OneToMany(targetEntity="Motor", mappedBy="barcos")
+     */
+    private $motores;
+
+    public function __construct() {
+        $this->motores = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Barco
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set marca
+     *
+     * @param string $marca
+     *
+     * @return Barco
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get marca
+     *
+     * @return string
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set modelo
+     *
+     * @param string $modelo
+     *
+     * @return Barco
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+
+        return $this;
+    }
+
+    /**
+     * Get modelo
+     *
+     * @return string
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * Set anio
+     *
+     * @param \DateTime $anio
+     *
+     * @return Barco
+     */
+    public function setAnio($anio)
+    {
+        $this->anio = $anio;
+
+        return $this;
+    }
+
+    /**
+     * Get anio
+     *
+     * @return \DateTime
+     */
+    public function getAnio()
+    {
+        return $this->anio;
+    }
+
+    /**
+     * Set estatus
+     *
+     * @param boolean $estatus
+     *
+     * @return Barco
+     */
+    public function setEstatus($estatus)
+    {
+        $this->estatus = $estatus;
+
+        return $this;
+    }
+
+    /**
+     * Get estatus
+     *
+     * @return bool
+     */
+    public function getEstatus()
+    {
+        return $this->estatus;
+    }
+}
+
---
+++ src/AppBundle/Entity/Motor.php
@@ -0,0 +1,288 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Motor
+ *
+ * @ORM\Table(name="motor")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MotorRepository")
+ */
+class Motor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="marca", type="string", length=100)
+     */
+    private $marca;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="modelo", type="string", length=100, nullable=true)
+     */
+    private $modelo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="calado", type="string", length=100, nullable=true)
+     */
+    private $calado;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="manga", type="string", length=100, nullable=true)
+     */
+    private $manga;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="eslora", type="string", length=100, nullable=true)
+     */
+    private $eslora;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="combustible", type="integer", nullable=true)
+     */
+    private $combustible;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="agua", type="integer", nullable=true)
+     */
+    private $agua;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="estatus", type="boolean")
+     */
+    private $estatus;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Barco", inversedBy="motores")
+     * @ORM\JoinColumn(name="idbarco", referencedColumnName="id")
+     */
+    private $barcos;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set marca
+     *
+     * @param string $marca
+     *
+     * @return Motor
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get marca
+     *
+     * @return string
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set modelo
+     *
+     * @param string $modelo
+     *
+     * @return Motor
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+
+        return $this;
+    }
+
+    /**
+     * Get modelo
+     *
+     * @return string
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * Set calado
+     *
+     * @param string $calado
+     *
+     * @return Motor
+     */
+    public function setCalado($calado)
+    {
+        $this->calado = $calado;
+
+        return $this;
+    }
+
+    /**
+     * Get calado
+     *
+     * @return string
+     */
+    public function getCalado()
+    {
+        return $this->calado;
+    }
+
+    /**
+     * Set manga
+     *
+     * @param string $manga
+     *
+     * @return Motor
+     */
+    public function setManga($manga)
+    {
+        $this->modelo = $manga;
+
+        return $this;
+    }
+
+    /**
+     * Get manga
+     *
+     * @return string
+     */
+    public function getManga()
+    {
+        return $this->manga;
+    }
+
+    /**
+     * Set eslora
+     *
+     * @param string $eslora
+     *
+     * @return Motor
+     */
+    public function setEslora($eslora)
+    {
+        $this->modelo = $eslora;
+
+        return $this;
+    }
+
+    /**
+     * Get eslora
+     *
+     * @return string
+     */
+    public function getEslora()
+    {
+        return $this->eslora;
+    }
+
+    /**
+     * Set combustible
+     *
+     * @param integer $combustible
+     *
+     * @return Motor
+     */
+    public function setCombustible($combustible)
+    {
+        $this->combustible = $combustible;
+
+        return $this;
+    }
+
+    /**
+     * Get combustible
+     *
+     * @return int
+     */
+    public function getCombustible()
+    {
+        return $this->combustible;
+    }
+
+    /**
+     * Set agua
+     *
+     * @param integer $agua
+     *
+     * @return Motor
+     */
+    public function setAgua($agua)
+    {
+        $this->combustible = $agua;
+
+        return $this;
+    }
+
+    /**
+     * Get agua
+     *
+     * @return int
+     */
+    public function getAgua()
+    {
+        return $this->agua;
+    }
+
+    /**
+     * Set estatus
+     *
+     * @param boolean $estatus
+     *
+     * @return Motor
+     */
+    public function setEstatus($estatus)
+    {
+        $this->estatus = $estatus;
+
+        return $this;
+    }
+
+    /**
+     * Get estatus
+     *
+     * @return bool
+     */
+    public function getEstatus()
+    {
+        return $this->estatus;
+    }
+}
+
