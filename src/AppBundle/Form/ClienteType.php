b38be749825715d27d35e5f4391577242dd3c7c8
Luiz <lkiora.a@hotmail.es>
2017-10-02 17:39:39 -0500

    quitando carpeta .idea

--- src/AppBundle/Form/ClienteType.php
+++ src/AppBundle/Form/ClienteType.php
@@ -16,7 +16,7 @@ class ClienteType extends AbstractType
 {
     /**
      * {@inheritdoc}
-     */
+     */ 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
