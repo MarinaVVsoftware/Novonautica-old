cefb33a69228880a05b12f3d710347739a89343a
Luis <lkiora.a@hotmail.es>
2017-11-21 01:02:51 -0500

    modulo servicios adicionales, listado y detalle completado, completando nuevo y editar

--- src/AppBundle/Form/MarinaHumedaCotizacionAdicionalType.php
+++ src/AppBundle/Form/MarinaHumedaCotizacionAdicionalType.php
@@ -56,12 +56,20 @@ class MarinaHumedaCotizacionAdicionalType extends AbstractType
             ));
 
         };
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) use ($formModifier) {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($formModifier) {
                 // this would be your entity, i.e. SportMeetup
                 $data = $event->getData();
-                $formModifier($event->getForm(), $data->getCliente());
+                $form = $event->getForm();
+
+                if($data->getId()){
+                    $form
+                        ->remove('cliente');
+
+                }else{
+
+                    $formModifier($event->getForm(), $data->getCliente());
+                }
+
             }
         );
         $builder->get('cliente')->addEventListener(
