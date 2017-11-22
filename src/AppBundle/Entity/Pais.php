9feda0bddc79b41cc941cae4291047a8859f8dbd
inrumi <inrumi@gmail.com>
2017-11-22 17:19:44 -0500

    Marcas y modelos para embarcaciones terminado
    reparado el tema falta guardar las imagenes
    
    Marcas y modelos para embarcaciones terminado
    
    Embarcacion falta agregar img, video, brochure, layout
    
    Subir imagenes de oceandeal a medias
    
    Marcas y modelos para embarcaciones terminado
    
    Embarcacion falta agregar img, video, brochure, layout
    
    Subir imagenes de oceandeal a medias
    
    reparado el tema falta guardar las imagenes
    
    Upload y delete de imagenes con dropzone
    
    Embarcacion Imagenes entidades unidas funcionando
    
    Brochures de embarcaciones funcionando
    
    Embarcaciones motores terminado, falta su listado
    
    Modulo ocean deal 99%, falta un icono en sidebar?
    
    WIP: Embarcaciones tiene paginacion
    
    Embarcaciones listo, falta una tabla hecha en js?

---
+++ src/AppBundle/Entity/Pais.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pais
+ *
+ * @ORM\Table(name="pais")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PaisRepository")
+ */
+class Pais
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=3)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="phonecode", type="integer")
+     */
+    private $phonecode;
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Pais
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Pais
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set phonecode
+     *
+     * @param integer $phonecode
+     *
+     * @return Pais
+     */
+    public function setPhonecode($phonecode)
+    {
+        $this->phonecode = $phonecode;
+
+        return $this;
+    }
+
+    /**
+     * Get phonecode
+     *
+     * @return int
+     */
+    public function getPhonecode()
+    {
+        return $this->phonecode;
+    }
+}
+
