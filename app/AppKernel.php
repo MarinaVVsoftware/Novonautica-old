8683c6874464bac46bf5940ad35318e03853560b
inrumi <inrumi@gmail.com>
2017-11-29 10:38:00 -0500

    Subir imagenes de oceandeal a medias

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,8 +21,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
+            new Oneup\UploaderBundle\OneupUploaderBundle(),
+            new AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
