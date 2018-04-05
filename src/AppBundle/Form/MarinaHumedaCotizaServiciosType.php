44168fc22c7db142f63d6a5f01575843552bf576
Johnn Mis <inrumi@gmail.com>
2018-04-05 11:45:43 -0500

    Ahora es obligatorio elegir prcio por dia y electricidad en MHC

--- src/AppBundle/Form/MarinaHumedaCotizaServiciosType.php
+++ src/AppBundle/Form/MarinaHumedaCotizaServiciosType.php
@@ -8,17 +8,13 @@
 
 namespace AppBundle\Form;
 
-
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use AppBundle\Entity\MarinaHumedaTarifa;
 
 class MarinaHumedaCotizaServiciosType extends AbstractType
 {
@@ -29,15 +25,10 @@ class MarinaHumedaCotizaServiciosType extends AbstractType
     {
 
         $builder
-            //->add('servicio')
-//            ->add('cantidad',null,[
-//                'attr' => ['class' => 'esdecimal']
-//            ])
             ->add('precio', EntityType::class, [
                 'class' => 'AppBundle:MarinaHumedaTarifa',
                 'label' => 'Precio',
                 'placeholder' => 'Seleccionar...',
-                'required' => false,
                 'choice_value' => 'costo',
                 'query_builder' => function (EntityRepository $er) {
                     return $er->createQueryBuilder('t')
@@ -49,7 +40,6 @@ class MarinaHumedaCotizaServiciosType extends AbstractType
                 'class' => 'AppBundle:MarinaHumedaTarifa',
                 'label' => 'Precio',
                 'placeholder' => 'Seleccionar...',
-                'required' => false,
                 'choice_value' => 'costo',
                 'query_builder' => function (EntityRepository $er) {
                     return $er->createQueryBuilder('t')
@@ -72,7 +62,6 @@ class MarinaHumedaCotizaServiciosType extends AbstractType
                     'class' => 'AppBundle:MarinaHumedaTarifa',
                     'label' => 'Precio',
                     'placeholder' => 'Seleccionar...',
-                    'required' => false,
                     'choice_value' => 'costo',
                     'query_builder' => function (EntityRepository $er) {
                         return $er->createQueryBuilder('t')
@@ -87,7 +76,6 @@ class MarinaHumedaCotizaServiciosType extends AbstractType
                         'class' => 'AppBundle:MarinaHumedaTarifa',
                         'label' => 'Precio',
                         'placeholder' => 'Seleccionar...',
-                        'required' => false,
                         'choice_value' => 'costo',
                         'query_builder' => function (EntityRepository $er) {
                             return $er->createQueryBuilder('t')
