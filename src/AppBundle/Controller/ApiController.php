3fb09be9fa802c423ab190899edf57cbd313c3b4
Luiz <lkiora.a@hotmail.es>
2018-05-24 16:41:31 -0500

    agregado condicion http, https en controlador api

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -31,44 +31,58 @@ class ApiController extends Controller
      */
     public function buscarEmbarcacionAction(Request $request)
     {
+        $headers = $request->headers->all();
+        $url=($headers['origin'][0] === 'http://www.oceandeal.com' || $headers['origin'][0] === 'https://www.oceandeal.com')?$headers['origin'][0]:'';
         $embarcaciones = $this->getDoctrine()->getManager()->getRepository('AppBundle:Embarcacion')->filtrarBarcos($request);
-        return $this->json($embarcaciones,200,['Access-Control-Allow-Origin'=> 'http://www.oceandeal.com']);
+        return $this->json($embarcaciones,200,['Access-Control-Allow-Origin'=> $url]);
     }
 
     /**
      * @Route("/marcas.json")
      * @Method("GET")
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function buscarMarcasAction()
+    public function buscarMarcasAction(Request $request)
     {
+        $headers = $request->headers->all();
+        $url=($headers['origin'][0] === 'http://www.oceandeal.com' || $headers['origin'][0] === 'https://www.oceandeal.com')?$headers['origin'][0]:'';
         $marcas = $this->getDoctrine()->getManager()->getRepository('AppBundle:EmbarcacionMarca')->encuentraMarcas();
-        return $this->json($marcas,200,['Access-Control-Allow-Origin'=> 'http://www.oceandeal.com']);
+        return $this->json($marcas,200,['Access-Control-Allow-Origin'=> $url]);
     }
 
     /**
      * @Route("/paises.json")
      * @Method("GET")
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function buscarPaisesAction()
+    public function buscarPaisesAction(Request $request)
     {
+        $headers = $request->headers->all();
+        $url=($headers['origin'][0] === 'http://www.oceandeal.com' || $headers['origin'][0] === 'https://www.oceandeal.com')?$headers['origin'][0]:'';
         $paises = $this->getDoctrine()->getManager()->getRepository('AppBundle:Pais')->encuentraPaises();
-        return $this->json($paises,200,['Access-Control-Allow-Origin'=> 'http://www.oceandeal.com']);
+        return $this->json($paises,200,['Access-Control-Allow-Origin'=> $url]);
     }
 
     /**
      * @Route("/anios.json")
      * @Method("GET")
      *
+     * @param Request $request
+     *
      * @return Response
      */
-    public function buscarAniosAction()
+    public function buscarAniosAction(Request $request)
     {
+        $headers = $request->headers->all();
+        $url=($headers['origin'][0] === 'http://www.oceandeal.com' || $headers['origin'][0] === 'https://www.oceandeal.com')?$headers['origin'][0]:'';
         $anios = $this->getDoctrine()->getManager()->getRepository('AppBundle:Embarcacion')->encuentraAniosUnicos();
-        return $this->json($anios,200,['Access-Control-Allow-Origin'=> 'http://www.oceandeal.com']);
+        return $this->json($anios,200,['Access-Control-Allow-Origin'=> $url]);
     }
 
 }
