66aefd273a98e24c283446a414aa7c1a1f2329df
Luiz <lkiora.a@hotmail.es>
2018-06-11 12:26:02 -0500

    Se cambió grafica de gantt en odt de fullCalendar a jsGanttImproved, incluye gráfico de actividades expandible #297. Agregado método en apiController para mostrar pdf. agregado estilo scroll gráfica de gantt

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -8,6 +8,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Embarcacion;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -79,6 +81,37 @@ class ApiController extends Controller
         $headers = $request->headers->all();
         $anios = $this->getDoctrine()->getManager()->getRepository('AppBundle:Embarcacion')->encuentraAniosUnicos();
         return $this->compruebaDominio($headers['origin'][0],$anios);
+    }
+
+    /**
+     * @Route("/{id}/brochure.pdf")
+     * @Method("GET")
+     *
+     * @param Embarcacion $embarcacion
+     * @param Request $request
+     *
+     * @return PdfResponse|Response
+     */
+    public function showBrochureAction(Request $request,Embarcacion $embarcacion)
+    {
+        $head = $this->renderView('embarcacion/pdf/head.html.twig');
+        $body = $this->renderView('embarcacion/pdf/body.html.twig', [
+            'title' => 'brochure.pdf',
+            'embarcacion' => $embarcacion
+        ]);
+
+        $options = [
+            'margin-top' => 24,
+            'margin-right' => 5,
+            'margin-left' => 5,
+            'margin-bottom' => 5,
+            'header-html' => utf8_decode($head),
+        ];
+
+        return new PdfResponse(
+            $this->get('knp_snappy.pdf')->getOutputFromHtml($body, $options),
+            'brochure.pdf', 'application/pdf', 'inline'
+        );
     }
 
     private function compruebaDominio($dominio,$objeto){
