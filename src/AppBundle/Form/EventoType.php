2feb6721d2c1b8caa60e10c0ce60e9fd54d57ac1
Luiz <lkiora.a@hotmail.es>
2018-06-21 17:42:08 -0500

    Reincorporacion de la agenda. Modificado formulario para agregar colores y visibilidad. Al guardarlo se registra usuario que lo creo con fecha y hora. Incluido permisos para ver, agregar, editar o borrar eventos. Al ser del tipo publico todos lo ven, al ser del tipo privado solo el usuario que lo creo lo puede ver. #325

--- src/AppBundle/Form/EventoType.php
+++ src/AppBundle/Form/EventoType.php
@@ -6,7 +6,11 @@ namespace AppBundle\Form;
 //use Doctrine\DBAL\Types\TimeType;
 //use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
@@ -59,7 +63,55 @@ class EventoType extends AbstractType
                 'label' => 'Descripción',
                 'attr' => ['rows' => 5],
                 'required' => false
-            ]);
+            ])
+
+
+        ;
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $evento = $event->getData();
+            $form = $event->getForm();
+
+            if($evento->getId() == null){
+                $form
+                    ->add('fondocolor',ColorType::class,[
+                        'label' => 'Color de fondo',
+                        'attr' => ['class' => 'input-color','value' => '#ffffff'],
+                        'empty_data' => '#ffffff'
+                    ])
+                    ->add('letracolor',ColorType::class,[
+                        'label' => 'Color de letra',
+                        'attr' => ['class' => 'input-color', 'value' => '#000000'],
+                        'empty_data' => '#000000'
+                    ])
+                    ->add('isPublico', ChoiceType::class, [
+                        'choices' => ['Privado' => 0, 'Público' => 1],
+                        'label' => 'Visibilidad',
+                        'expanded' => true,
+                        'multiple' => false,
+                        'data' => 0
+//                        'choice_attr' => function ($val, $key, $index) {return ['class' => 'opcion' . strtolower($key)];}
+                        ]);
+            }else{
+                $form
+                    ->add('fondocolor',ColorType::class,[
+                        'label' => 'Color de fondo',
+                        'attr' => ['class' => 'input-color'],
+                        'empty_data' => '#ffffff'
+                    ])
+                    ->add('letracolor',ColorType::class,[
+                        'label' => 'Color de letra',
+                        'attr' => ['class' => 'input-color'],
+                        'empty_data' => '#000000'
+                    ])
+                    ->add('isPublico', ChoiceType::class, [
+                        'choices' => ['Privado' => 0, 'Público' => 1],
+                        'label' => 'Visibilidad',
+                        'expanded' => true,
+                        'multiple' => false,
+                    ]);
+            }
+        });
     }
     
     /**
