2fda4fb516e1eafd9bbfe3e7bee261fb32fbfde7
Johnn Mis <inrumi@gmail.com>
2018-07-05 12:57:51 -0500

    Ahora si se crean las entidades de tienda

--- src/AppBundle/Controller/Tienda/VentaController.php
+++ src/AppBundle/Controller/Tienda/VentaController.php
@@ -70,12 +70,11 @@ class VentaController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            /*$em = $this->getDoctrine()->getManager();
+            $em = $this->getDoctrine()->getManager();
             $em->persist($venta);
-            $em->flush();*/
+            $em->flush();
 
-//            return $this->redirectToRoute('tienda_venta_new');
-            dump($venta);
+            return $this->redirectToRoute('tienda_venta_new');
         }
 
         return $this->render(
