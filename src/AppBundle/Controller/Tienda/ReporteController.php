d43c7a8175643b213d7e466c26b789b2842b8762
Luiz <lkiora.a@hotmail.es>
2018-10-01 12:07:41 -0500

    Tienda y astillero: Cambiando botón exporta excel de consulta por botón generador de dataTable.

--- src/AppBundle/Controller/Tienda/ReporteController.php
+++ src/AppBundle/Controller/Tienda/ReporteController.php
@@ -62,87 +62,23 @@ class ReporteController extends AbstractController
                 'attr' => ['class' => 'btn-xs btn-azul pull-right no-loading'],
                 'label' => 'Buscar'
             ])
-            ->add('xls', SubmitType::class, [
-                'attr' => ['class' => 'btn-xs btn-verde no-loading'],
-                'label' => 'xls'
-            ])
             ->getForm();
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()){
             $datos = $form->getData();
             $idproducto = $datos['producto'] ? $datos['producto']->getId() : '0';
-            if($form->get('buscar')->isCLicked()){
-                $em = $this->getDoctrine()->getManager();
-                $ventas = $em->getRepository('AppBundle:Tienda\Venta\Concepto')
-                             ->getReporteVentas($idproducto,
-                                                $datos['inicio']->format('Y-m-d'),
-                                                $datos['fin']->format('Y-m-d')
-                             );
-            }else{
-                return $this->redirectToRoute('reporte_store_venta_data', [
-                    'idproducto' => $idproducto,
-                    'inicio' => $datos['inicio']->format('Y-m-d'),
-                    'fin' => $datos['fin']->format('Y-m-d')
-                ]);
-            }
+            $em = $this->getDoctrine()->getManager();
+            $ventas = $em->getRepository('AppBundle:Tienda\Venta\Concepto')
+                ->getReporteVentas($idproducto,
+                    $datos['inicio']->format('Y-m-d'),
+                    $datos['fin']->format('Y-m-d')
+                );
         }
         return $this->render('tienda/reporte/venta.html.twig',[
             'ventas' => $ventas,
             'title' => 'Reporte tienda',
             'form' => $form->createView()
         ]);
-    }
-
-    /**
-     * @Route("/venta/{idproducto}/{inicio}/{fin}", name="reporte_store_venta_data")
-     * @Method({"GET", "POST"})
-     * @param $idproducto
-     * @param $inicio
-     * @param $fin
-     * @return StreamedResponse
-     */
-    public function generateCsvAction($idproducto,$inicio,$fin)
-    {
-        $response = new StreamedResponse();
-        $response->setCallback(function () use($idproducto,$inicio,$fin) {
-            $handle = fopen('php://output', 'w+');
-            $auxTitulos = [
-                'Fecha venta',
-                'Producto',
-                'Cantidad',
-                'Precio',
-                'Sub-Total',
-                'IVA',
-                'Descuento',
-                'Total'
-            ];
-            $titulos = [];
-            foreach ($auxTitulos as $auxTitulo) {
-                array_push($titulos, mb_convert_encoding($auxTitulo, 'UTF-16LE', 'UTF-8'));
-            }
-            fputcsv($handle, $titulos);
-            $em = $this->getDoctrine()->getManager();
-            $ventas = $em->getRepository('AppBundle:Tienda\Venta\Concepto')
-                         ->getReporteVentas($idproducto,$inicio,$fin);
-            foreach ($ventas as $dato) {
-                fputcsv($handle, [
-                        $dato['fecha'],
-                        $dato['producto'],
-                        $dato['cantidad'],
-                        $dato['precio'],
-                        $dato['subtotal'],
-                        $dato['iva'],
-                        $dato['descuento'],
-                        $dato['total'],
-                    ]);
-            }
-            fclose($handle);
-        });
-        $response->setStatusCode(200);
-        $response->setCharset('UTF-8');
-        $response->headers->set('Content-Type', 'text/csv');
-        $response->headers->set('Content-Disposition', 'attachment; filename="TiendaEjemplo.csv"');
-        return $response;
     }
 }
