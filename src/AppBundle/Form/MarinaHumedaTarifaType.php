7ffe1089e519a5d18d1cd201c29d114b2be76511
Johnn Mis <inrumi@gmail.com>
2019-02-06 11:38:52 -0500

    Se removieron los campos CPS y CU de marina, se registraran de otra forma

--- src/AppBundle/Form/MarinaHumedaTarifaType.php
+++ src/AppBundle/Form/MarinaHumedaTarifaType.php
@@ -2,35 +2,15 @@
 
 namespace AppBundle\Form;
 
-use AppBundle\Entity\Contabilidad\Facturacion\Concepto\ClaveProdServ;
-use AppBundle\Entity\Contabilidad\Facturacion\Concepto\ClaveUnidad;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
-use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\NotNull;
 
 class MarinaHumedaTarifaType extends AbstractType
 {
-    /**
-     * @var EntityManagerInterface
-     */
-    private $entityManager;
-
-    public function __construct(
-        EntityManagerInterface $entityManager
-    ) {
-        $this->entityManager = $entityManager;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -56,34 +36,6 @@ class MarinaHumedaTarifaType extends AbstractType
                 'label' => 'Descripción',
                 'required' => false
             ]);
-
-        $builder->addEventListener(
-            FormEvents::PRE_SET_DATA,
-            function (FormEvent $event) {
-                $tarifa = $event->getData();
-
-                $this->createClaveUnidadField($event->getForm(), $tarifa->getClaveUnidad());
-                $this->createProdServField($event->getForm(), $tarifa->getClaveProdServ());
-            }
-        );
-
-        $builder->addEventListener(
-            FormEvents::PRE_SUBMIT,
-            function (FormEvent $event) {
-                $data = $event->getData();
-
-                $claveUnidad = array_key_exists('claveUnidad', $data)
-                    ? $this->entityManager->getRepository(ClaveUnidad::class)->find($data['claveUnidad'])
-                    : null;
-
-                $claveProdServ = array_key_exists('claveProdServ', $data)
-                    ? $this->entityManager->getRepository(ClaveProdServ::class)->find($data['claveProdServ'])
-                    : null;
-
-                $this->createClaveUnidadField($event->getForm(), $claveUnidad);
-                $this->createProdServField($event->getForm(), $claveProdServ);
-            }
-        );
     }
 
     /**
@@ -102,47 +54,5 @@ class MarinaHumedaTarifaType extends AbstractType
     public function getBlockPrefix()
     {
         return 'appbundle_marinahumedatarifa';
-    }
-
-    private function createClaveUnidadField(FormInterface $form, ClaveUnidad $claveUnidad = null)
-    {
-        $clavesUnidades = null === $claveUnidad ? [] : [$claveUnidad];
-
-        $form->add(
-            'claveUnidad',
-            EntityType::class,
-            [
-                'class' => ClaveUnidad::class,
-                'choice_label' => 'nombre',
-                'choices' => $clavesUnidades,
-                'required' => true,
-                'attr' => ['required' => 'required'],
-                'constraints' => [
-                    new NotNull(['message' => 'Por favor selecciona un valor']),
-                    new NotBlank(['message' => 'Por favor selecciona un valor']),
-                ],
-            ]
-        );
-    }
-
-    private function createProdServField(FormInterface $form, ClaveProdServ $claveProdServ = null)
-    {
-        $claveProdServs = null === $claveProdServ ? [] : [$claveProdServ];
-
-        $form->add(
-            'claveProdServ',
-            EntityType::class,
-            [
-                'class' => ClaveProdServ::class,
-                'choice_label' => 'descripcion',
-                'choices' => $claveProdServs,
-                'required' => true,
-                'attr' => ['required' => 'required'],
-                'constraints' => [
-                    new NotNull(['message' => 'Por favor selecciona un valor']),
-                    new NotBlank(['message' => 'Por favor selecciona un valor']),
-                ],
-            ]
-        );
     }
 }
